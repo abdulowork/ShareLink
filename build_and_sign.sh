1f8dee23ce20d781d23c26a9ad5bb34c9b2b32d9
Timofey Solonin <abdulowork@gmail.com>
2024-03-16 16:53:38 +0100

    Add signing keychain to the search path

--- build_and_sign.sh
+++ build_and_sign.sh
@@ -19,6 +19,7 @@ security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 
 security import "$APP_IDENTITY_PATH" -P "$APP_SIGNING_IDENTITY_PASSWORD" -A -t cert -f pkcs12 -k "$KEYCHAIN_PATH"
 security import "$FINDER_EXTENSION_IDENTITY_PATH" -P "$FINDER_EXTENSION_SIGNING_IDENTITY_PASSWORD" -A -t cert -f pkcs12 -k "$KEYCHAIN_PATH"
+security list-keychains -d user | xargs security list-keychains -d user -s "$KEYCHAIN_PATH"
 security set-key-partition-list -S apple-tool:,apple: -k "$KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 
 ARCHIVE_NAME=output
